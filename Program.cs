66c77d552e547b35f6f199b9294e4f445cfd44df
Maksym Havryliuk <maks.havryliuk@gmail.com>
2018-05-26 12:34:04 +0300

    Refactoring the application.

--- Program.cs
+++ Program.cs
@@ -2,34 +2,45 @@
 using System.Net;
 using System.Diagnostics;
 
-/* Write a program that downloads a file from Internet and stores it the current directory.
+/** <remark>
+ * Write a program that downloads a file from Internet and stores it the current directory.
  * Find in Google how to download files in C#. Be sure to catch all exceptions and to free any used resources in the finally block.
  * 
  * Напишите программу, загружающую файл из Интернета и сохраняет его в текущем каталоге. Найти в Google, как загрузить файлы в C#. 
- * Обязательно воспользуйтесь всеми исключениями и освободите любые использованные ресурсы в блоке finally. */
+ * Обязательно воспользуйтесь всеми исключениями и освободите любые использованные ресурсы в блоке finally.
+ </remark> */
 
 namespace _20180306_Task4_Exception
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private static void Main()
         {
             WebClient download = new WebClient();
 
             Console.Write("Paste URL: ");
-            string fileUrl = Console.ReadLine();   // https://i.ytimg.com/vi/1P9hLB67YqE/maxresdefault.jpg
+            string fileUrl = Console.ReadLine();           // https://i.ytimg.com/vi/1P9hLB67YqE/maxresdefault.jpg
 
             Console.Write("Enter path and filename: ");
-            string pathAndFilename = Console.ReadLine();           // D:\photo.jpg
+            string pathAndFilename = Console.ReadLine();   // D:\photo.jpg
 
             try
             {
-                download.DownloadFile(fileUrl, pathAndFilename);   // Download file
+                download.DownloadFile(
+                    fileUrl ?? throw new InvalidOperationException(),
+                    pathAndFilename ?? throw new InvalidOperationException()
+                    );
+
                 Console.WriteLine("File downloaded.");
-                Process.Start(pathAndFilename);                    // Open file
+
+                Process.Start(pathAndFilename);            // Open file
                 Console.WriteLine("File opened.");
             }
             catch (WebException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            catch (InvalidOperationException ex)
             {
                 Console.WriteLine("Error: " + ex.Message);
             }
